18c11b8f83439c7004ab0cef40329b00e3480817
edit0 <akdmadl34@naver.com>
2020-09-15 02:10:56 +0900

    now_location -> starting point or destination / navermaps

--- delivery/app/src/main/java/com/example/delivery/layout2_first.java
+++ delivery/app/src/main/java/com/example/delivery/layout2_first.java
@@ -9,12 +9,15 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.PointF;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -38,6 +41,7 @@ import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import org.json.JSONException;
@@ -361,78 +365,120 @@ public class layout2_first extends FragmentActivity
     @UiThread
     @Override
     public void onMapReady(@NonNull final NaverMap naverMap) {
-        final InfoWindow infoWindow = new InfoWindow();
-        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(getApplication()) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "목적지";
-            }
-        });
-        final InfoWindow infoWindow1 = new InfoWindow();
-        infoWindow1.setAdapter(new InfoWindow.DefaultTextAdapter(getApplication()) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "출발지";
-            }
-        });
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
 
-        final Geocoder g = new Geocoder(this);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                final double lat = location.getLatitude();
+                final double lng = location.getLongitude();
 
-        UiSettings uiSettings = naverMap.getUiSettings();
-        uiSettings.setCompassEnabled(true);
-        uiSettings.setLocationButtonEnabled(true);
-        naverMap.setLocationSource(locationSource);
+                final Marker marker = new Marker();
+                final Marker marker1 = new Marker();
 
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    boolean success = jsonObject.getBoolean("success"); //php보면 response가 success면 ㄱㄱ
-                    if(success){ //회원등록에 성공한 경우
-                        Double num1= 0.0;
-                        Double num2 = 0.0;
-                        Double num3 = 0.0;
-                        Double num4 = 0.0;
+                final InfoWindow infoWindow = new InfoWindow();
+                infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(getApplication()) {
+                    @NonNull
+                    @Override
+                    public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                        return "목적지";
+                    }
+                });
+                final InfoWindow infoWindow1 = new InfoWindow();
+                infoWindow1.setAdapter(new InfoWindow.DefaultTextAdapter(getApplication()) {
+                    @NonNull
+                    @Override
+                    public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                        return "출발지";
+                    }
+                });
 
-                        if(d_type.equals("0")){
-                            o_lat = jsonObject.getDouble("o_lat");
-                            o_long = jsonObject.getDouble("o_long");
-                            num1=o_lat;
-                            num2=o_long;
-                            u_lat = jsonObject.getDouble("u_lat");
-                            u_long = jsonObject.getDouble("u_long");
-                            num3=u_lat;
-                            num4 = u_long;
-                        }
-                        else{
-                            o_lat = jsonObject.getDouble("o_lat");
-                            o_long = jsonObject.getDouble("o_long");
-                            num3=o_lat;
-                            num4=o_long;
-                            u_lat = jsonObject.getDouble("u_lat");
-                            u_long = jsonObject.getDouble("u_long");
-                            num1=u_lat;
-                            num2 = u_long;
-                        }
 
-                        final Marker marker = new Marker();
-                        final Marker marker1 = new Marker();
-                        marker.setPosition(new com.naver.maps.geometry.LatLng(num1,num2));
-                        marker.setMap(naverMap);
+                final Geocoder g = new Geocoder(getApplication());
 
-                        marker1.setPosition(new com.naver.maps.geometry.LatLng(num3,num4));
-                        marker1.setMap(naverMap);
+                UiSettings uiSettings = naverMap.getUiSettings();
+                uiSettings.setCompassEnabled(true);
+                uiSettings.setLocationButtonEnabled(true);
+                naverMap.setLocationSource(locationSource);
 
-                        infoWindow.open(marker);
-                        infoWindow1.open(marker1);
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success"); //php보면 response가 success면 ㄱㄱ
+                            if(success){ //회원등록에 성공한 경우
 
-                        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new com.naver.maps.geometry.LatLng(num3,num4))
-                                .animate(CameraAnimation.Fly, 3000);
-                        naverMap.moveCamera(cameraUpdate);
+                                List<Address> a = null;
+
+
+                                Double num1= 0.0;
+                                Double num2 = 0.0;
+                                Double num3 = 0.0;
+                                Double num4 = 0.0;
+
+                                if(d_type.equals("0")){
+                                    o_lat = jsonObject.getDouble("o_lat");
+                                    o_long = jsonObject.getDouble("o_long");
+                                    num1=o_lat;
+                                    num2=o_long;
+                                    u_lat = jsonObject.getDouble("u_lat");
+                                    u_long = jsonObject.getDouble("u_long");
+                                    num3=u_lat;
+                                    num4 = u_long;
+                                }
+                                else{
+                                    o_lat = jsonObject.getDouble("o_lat");
+                                    o_long = jsonObject.getDouble("o_long");
+                                    num3=o_lat;
+                                    num4=o_long;
+                                    u_lat = jsonObject.getDouble("u_lat");
+                                    u_long = jsonObject.getDouble("u_long");
+                                    num1=u_lat;
+                                    num2 = u_long;
+                                }
+
+
+                                marker.setPosition(new com.naver.maps.geometry.LatLng(num1,num2));
+                                marker.setMap(naverMap);
+
+                                marker1.setPosition(new com.naver.maps.geometry.LatLng(num3,num4));
+                                marker1.setMap(naverMap);
+
+                                infoWindow.open(marker);
+                                infoWindow1.open(marker1);
+
+                                CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new com.naver.maps.geometry.LatLng(num3,num4))
+                                        .animate(CameraAnimation.Fly, 3000);
+                                naverMap.moveCamera(cameraUpdate);
+
+                                final Double f_num1 = num1;
+                                final Double f_num2 = num2;
+                                final Double f_num3 = num3;
+                                final Double f_num4 = num4;
+
+                                a = g.getFromLocation(lat,lng,1);
+                                final String b = a.get(0).getAddressLine(0);
+                                marker.setOnClickListener(new Overlay.OnClickListener() {//목적지
+                                    @Override
+                                    public boolean onClick(@NonNull Overlay overlay) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse("nmap://route/car?slat="+lat+"&slng="+lng+"&sname="+b+"&dlat="+f_num1+"&dlng="+f_num2+"&dname="+u_address+"&appname=com.example.myapp")));
+                                        return false;
+                                    }
+                                });
+
+                                marker1.setOnClickListener(new Overlay.OnClickListener() {//출발지
+                                    @Override
+                                    public boolean onClick(@NonNull Overlay overlay) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse("nmap://route/car?slat="+lat+"&slng="+lng+"&sname="+b+"&dlat="+f_num3+"&dlng="+f_num4+"&dname="+s_address+"&appname=com.example.myapp")));
+                                        return false;
+                                    }
+                                });
+
+
 
 
                         /*LatLng location1 = new LatLng(num1, num2);
@@ -450,23 +496,60 @@ public class layout2_first extends FragmentActivity
                         markerOptions.position(location);
                         googleMap.addMarker(markerOptions);
                         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,16));*/
+                            }
+                            //실패한 경우
+                            else{
+                                Toast.makeText(getApplicationContext(),"오류",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException | IOException e) {
+                            e.printStackTrace();
+                        }
+
                     }
-                    //실패한 경우
-                    else{
-                        Toast.makeText(getApplicationContext(),"오류",Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                };
+
+                //서버로 Volley를 이용해서 요청을 함
+                layout2_first_map_db registerRequest = new layout2_first_map_db(d_type,u_number,u_address,s_number,s_address, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(layout2_first.this);
+                queue.add(registerRequest);
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            public void onProviderDisabled(String provider) {
 
             }
         };
 
-        //서버로 Volley를 이용해서 요청을 함
-        layout2_first_map_db registerRequest = new layout2_first_map_db(d_type,u_number,u_address,s_number,s_address, responseListener);
-        RequestQueue queue = Volley.newRequestQueue(layout2_first.this);
-        queue.add(registerRequest);
+        // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+
+
+
+
+
+
         /*Toast.makeText(getApplicationContext(),""+o_lat+o_long+"",Toast.LENGTH_SHORT).show();*/
 
 
@@ -476,6 +559,26 @@ public class layout2_first extends FragmentActivity
                 "위도: " + coord.latitude + ", 경도: " + coord.longitude,
                 Toast.LENGTH_SHORT).show();*/
     }
+
+    /*@Override
+    public boolean onClick(@NonNull Overlay overlay) {
+        if(overlay instanceof Marker){
+            Intent Sharing_intent = new Intent(Intent.ACTION_SEND);
+            Sharing_intent.setType("text/plain");
+
+
+
+
+            *//*String a = "https://map.naver.com/v5/search/"+re_set_title+"?c=14104623.8586647,4511836.5253034,15,0,0,0,dh";*//*
+            String a = "https://map.naver.com/v5/search/";
+            Sharing_intent.putExtra(Intent.EXTRA_TEXT, "nmap://search/bus?query=222&appname=com.example.myapp");
+
+            startActivity(Sharing_intent);
+
+            return true;
+        }
+        return false;
+    }*/
 
     /*@Override
     public boolean onMyLocationButtonClick() {
