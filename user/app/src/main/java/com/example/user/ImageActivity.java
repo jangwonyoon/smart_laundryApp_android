e51ee3b99e1a42a93aa8367dc2f1b0484a684f5a
TheJang <wkddnjs7310@naver.com>
2020-10-24 19:50:09 +0900

    user_frag_first java acitonBar add

--- user/app/src/main/java/com/example/user/ImageActivity.java
+++ user/app/src/main/java/com/example/user/ImageActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -33,15 +34,26 @@ import java.util.ArrayList;
 
 public class ImageActivity extends AppCompatActivity {
 
+    String user_name1,user_address1,user_id1,user_address_detail1;
+    double user_lat1,user_long1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
+        Intent intent = getIntent();
+        user_name1 = intent.getStringExtra("user_name");
+        user_address1 = intent.getStringExtra("user_address");
+        user_lat1 = intent.getDoubleExtra("user_lat",0.0);
+        user_long1 = intent.getDoubleExtra("user_long",0.0);
+        user_id1 = intent.getStringExtra("user_id");
+        user_address_detail1 = intent.getStringExtra("user_address_detail");
+
         ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-
+        actionBar.setTitle("매장");
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFF4472C4));
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         ViewPager pager = findViewById(R.id.pager);
         //캐싱을 해놓을 프래그먼트 개수
@@ -49,7 +61,6 @@ public class ImageActivity extends AppCompatActivity {
 
         //getSupportFragmentManager로 프래그먼트 참조가능
         FragmentStatePagerAdapter adapter = new MoviePagerAdapter(getSupportFragmentManager());
-
 
         /*adapter.addItem(fragment1);*/
 
@@ -62,10 +73,28 @@ public class ImageActivity extends AppCompatActivity {
         pager.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                Intent intent = new Intent(getApplicationContext(), user_main1.class);
+                intent.putExtra("user_name",user_name1);
+                intent.putExtra("user_address",user_address1);
+                intent.putExtra("user_lat",user_lat1);
+                intent.putExtra("user_long",user_long1);
+                intent.putExtra("user_id",user_id1);
+                intent.putExtra("user_address_detail",user_address_detail1);
+                startActivity(intent);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     //어댑터 안에서 각각의 아이템을 데이터로서 관리한다
+
     class MoviePagerAdapter extends FragmentStatePagerAdapter {
         ArrayList<Fragment> items = new ArrayList<Fragment>();
-
         public MoviePagerAdapter(FragmentManager fm) {
             super(fm);
         }
@@ -84,4 +113,6 @@ public class ImageActivity extends AppCompatActivity {
             return items.size();
         }
     }
+
+
 }
